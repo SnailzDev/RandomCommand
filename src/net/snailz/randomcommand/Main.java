88d77c9f0ed95defcb212223f0153929a41e4d1d
BuildTools <unconfigured@null.spigotmc.org>
2015-10-16 08:51:40 -0700

    add option to not use Percentages

--- src/net/snailz/randomcommand/Main.java
+++ src/net/snailz/randomcommand/Main.java
@@ -21,6 +21,14 @@ public class Main extends JavaPlugin implements CommandExecutor{
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
         if (command.getName().equalsIgnoreCase("RC")){
             List<String> list = this.getConfig().getStringList(args[0]);
+            if (this.getConfig().getBoolean("use%") == false){
+                    Random random = new Random();
+                    String cmd = list.get(random.nextInt(list.size()));
+                    cmd = cmd.replace("{name}", args[1]);
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
+                    return true;
+            }
+            if (this.getConfig().getBoolean("use%") == true){
             int randomint = (int) (Math.random() * 100);
             for (String cmd : list){
                 cmd = cmd.replace("{name}", args[1]);
@@ -29,9 +37,8 @@ public class Main extends JavaPlugin implements CommandExecutor{
                 if (percent > randomint){
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd_list[1]);
                     return true;
-                } else{
-                    continue;
                 }
+            }
             }
             
         }
