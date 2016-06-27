8598878cb014df421b0173d81d0b5d5c944b0a9e
BuildTools <unconfigured@null.spigotmc.org>
2016-06-27 04:16:13 -0700

    Added more than two commands on one option and fixed some bogs

--- src/net/snailz/randomcommand/Main.java
+++ src/net/snailz/randomcommand/Main.java
@@ -8,6 +8,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -20,32 +21,54 @@ public class Main extends JavaPlugin implements CommandExecutor{
         int CVU = this.getConfig().getInt("configver");
         if (configver != CVU){
             getLogger().severe("NEW CONFIG NEEDED. COPY ALL COMMAND LISTS, DELETE CONFIG, AND RESTART SERVER!");
+            Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("RandomCommand");
+            Bukkit.getServer().getPluginManager().disablePlugin(plugin);
         }
     }
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        if (command.getName().equalsIgnoreCase("RC")){
+        if (command.getName().equalsIgnoreCase("rc")){
             if (args.length == 0){
                 sender.sendMessage(ChatColor.GREEN + "RandomCommand Help");
                 sender.sendMessage(ChatColor.GREEN + "/rc <list> <playername>");
-                sender.sendMessage(ChatColor.GREEN + "use: execute a random command from a list that you put as the 1st argument with {name} replaced with the player name that you put as the 2nd argument");
+                sender.sendMessage(ChatColor.GREEN + "Use: execute a random command from a list that you put as the 1st argument with {name} replaced with the player name that you put as the 2nd argument");
                 return true;
             }
             List<String> list = this.getConfig().getStringList(args[0]);
             if (this.getConfig().getBoolean("use%") == false){
                     Random random = new Random();
                     String cmd = list.get(random.nextInt(list.size()));
-                    if (cmd.contains("{name}")){
-                    cmd = cmd.replace("{name}", args[1]);
-                    }
-                    if (cmd.toLowerCase().contains(this.getConfig().getString("commandsplitcharacter"))){
-                        String[] cmds = cmd.split(this.getConfig().getString("commandsplitcharacter"));
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmds[0]);
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmds[1]);
+                    String CSC = this.getConfig().getString("commandsplitcharacter");
+                    if (cmd.toLowerCase().contains(CSC)){
+                        String[] cmds = cmd.split(CSC);
+                        int cmdslength = cmds.length;
+                        for (int x = 0;x < cmdslength; x++){
+                            String currentcmd = cmds[x];
+                            if (currentcmd.contains("{name}") && args.length <= 1){
+                                sender.sendMessage(ChatColor.YELLOW + "You must add a player name to the command for this list!");
+                                sender.sendMessage(ChatColor.YELLOW + "Format: /rc <listname> [playername]");
+                            }
+                        }
+                        for (int x = 0;x < cmdslength; x++){
+                            if (cmds[x].contains("{name}")){
+                                cmds[x].replace("{name}", args[1]);
+                            }
+                        }
+                        for (int x = 0;x < cmdslength; x++){
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmds[x]);
+                        }
                         return true;
                     }
                     else{
+                    if (cmd.contains("{name}")){
+                            if (args.length == 1){
+                                sender.sendMessage(ChatColor.YELLOW + "You must add a player name to the command for this list!");
+                                sender.sendMessage(ChatColor.YELLOW + "Format: /rc <listname> [playername]");
+                                return true;
+                            }
+                            cmd = cmd.replace("{name}", args[1]);
+                        }
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd);
                     return true;
                     }
@@ -53,21 +76,41 @@ public class Main extends JavaPlugin implements CommandExecutor{
             if (this.getConfig().getBoolean("use%") == true){
             int randomint = (int) (Math.random() * 100);
             for (String cmd : list){
-                if (cmd.contains("{name}")){
-                    cmd = cmd.replace("{name}", args[1]);
-                    }
                 String[] cmd_list = cmd.split(":");
                 int percent = Integer.parseInt(cmd_list[0]);
                 if (percent > randomint){
-                    if (cmd_list[1].toLowerCase().contains(this.getConfig().getString("commandsplitcharacter"))){
-                        String[] cmds = cmd_list[1].split(this.getConfig().getString("commandsplitcharacter"));
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmds[0]);
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmds[1]);
+                    String CSC = this.getConfig().getString("commandsplitcharacter");
+                    if (cmd.toLowerCase().contains(CSC)){
+                        String[] cmds = cmd_list[1].split(CSC);
+                        int cmdslength = cmds.length;
+                        for (int x = 0;x < cmdslength; x++){
+                            String currentcmd = cmds[x];
+                            if (currentcmd.contains("{name}") && args.length <= 1){
+                                sender.sendMessage(ChatColor.YELLOW + "You must add a player name to the command for this list!");
+                                sender.sendMessage(ChatColor.YELLOW + "Format: /rc <listname> [playername]");
+                            }
+                        }
+                        for (int x = 0;x < cmdslength; x++){
+                            if (cmds[x].contains("{name}")){
+                                cmds[x].replace("{name}", args[1]);
+                            }
+                        }
+                        for (int x = 0;x < cmdslength; x++){
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmds[x]);
+                        }
                         return true;
                     }
                     else{
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd_list[1]);
-                    return true;
+                        if (cmd_list[1].contains("{name}")){
+                            if (args.length == 1){
+                                sender.sendMessage(ChatColor.YELLOW + "You must add a player name to the command for this list!");
+                                sender.sendMessage(ChatColor.YELLOW + "Format: /rc <listname> [playername]");
+                                return true;
+                            }
+                            cmd_list[1] = cmd_list[1].replace("{name}", args[1]);
+                        }
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd_list[1]);
+                        return true;
                     }
                 }
             }
